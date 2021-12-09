d1faf78a7f1283810ddf5f68309a5737e4213de3
Oskari <oskari.niskanen97@gmail.com>
2021-12-09 22:32:14 +0200

    First commit. Added base program code and README.

---
+++ README.md
@@ -0,0 +1,6 @@
+This project is a hobby project that makes a plush toy rat speak random things with text-to-speech.
+Program is being run on Raspberry Pi and called every hour with Crontab.
+When program is being ran, it randomly calls an API and speaks out the result. Currently there are 4 possible APIs.
+They are current day's sunrise and sunset time, programming quote, your mom joke and an inspirational quote.
+
+You are free to use this project as you wish.
---
+++ src/run_rat.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+"""
+Program that fetches texts and speaks it aloud.
+"""
+
+import requests
+import pyttsx3
+import random
+import time
+import logging
+from datetime import datetime, timedelta
+
+__author__ = "Oskari Niskanen"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+engine = pyttsx3.init()  # TTS engine creation
+# Slowing down the speech rate by 5%
+engine.setProperty('rate', 190)
+# Counter to limit api call repeating if requests fail.
+api_call_count = 0
+# Datetime format
+formatfrom = "%Y-%m-%dT%H:%M:%S+00:00"
+# Configuring logger
+logging.basicConfig(level=logging.INFO)
+
+
+def read_day_statistics():
+    """ Reads sunrise and sunset times out loud. """
+
+    logging.info("Reading sunset and sunrise times.")
+    url = "https://api.sunrise-sunset.org/json?lat=61.49911&lng=23.78712&date=today&formatted=0"
+    response = request_api(url)
+    # Checking that correct key is in response
+    if 'results' in response:
+        sunrise_utc = response['results']['sunrise']
+        sunset_utc = response['results']['sunset']
+
+        # Converting UTC times to GMT+2
+        sunrise = datetime.strptime(
+            sunrise_utc, formatfrom) + timedelta(hours=2)
+        sunset = datetime.strptime(sunset_utc, formatfrom) + timedelta(hours=2)
+
+        # Extracting hours and minutes
+        rise_hour = str(sunrise.hour)
+        rise_minute = str(sunrise.minute)
+        set_hour = str(sunset.hour)
+        set_minute = str(sunset.minute)
+        # Getting time of day
+        rise_am_pm = sunrise.strftime("%p")
+        set_am_pm = sunset.strftime("%p")
+
+        # Constructing cohesive sentence.
+        sun_statistics = ("Sun rises at " + rise_minute + "past" + rise_hour + " " + rise_am_pm +
+                          " and sets at " + set_minute + " past " + set_hour + " " + set_am_pm)
+
+        # Reading sun statistics for current day.
+        read_string(sun_statistics)
+
+
+def read_programming_quote():
+    """ Reads a computer programming quote out loud. """
+
+    logging.info("Reading programming quote.")
+    url = "https://programming-quotes-api.herokuapp.com/quotes/random"
+    response = request_api(url)
+    # Checking that correct key is in response
+    if 'en' in response:
+        read_string(response['en'])
+
+
+def read_inspirational_quote():
+    """ Reads a famous quote out loud."""
+
+    logging.info("Reading inspirational quote.")
+    url = "https://inspiration.goprogram.ai"
+    response = request_api(url)
+    # Checking that correct key is in response
+    if 'quote' in response:
+        read_string(response['quote'])
+
+
+def read_joke():
+    """ Reads a joke out loud. """
+
+    logging.info("Reading joke.")
+    joke_url = "https://yomomma-api.herokuapp.com/jokes"
+    response = request_api(joke_url)
+    # Checking that correct key is in response
+    if 'joke' in response:
+        read_string(response['joke'])
+
+
+def choose_phrase():
+    """ Chooses randomly a function to call. """
+
+    logging.info("Choosing phrase.")
+    # Possible functions to call
+    possible_functions = [read_inspirational_quote,
+                          read_joke, read_programming_quote, read_day_statistics]
+    rand_num = random.randint(0, 3)
+    # Calling random function
+    possible_functions[rand_num]()
+
+
+def request_api(url):
+    """ Sends a request to api and returns data as JSON. """
+
+    global api_call_count
+    r = requests.get(url)
+    # If status is OK, then return response as json.
+    if (r.status_code == 200):
+        return r.json()
+    elif (api_call_count < 5):
+        print("API response was not OK from " + url)
+        # Else wait for 30 seconds and try again.
+        time.sleep(30)
+        api_call_count = api_call_count + 1
+        choose_phrase()
+
+
+def read_string(sentence):
+    """ Reads a given sentence out loud. """
+
+    engine.say(sentence)
+    engine.runAndWait()
+
+
+def main():
+    """ Main entry point of the app """
+
+    choose_phrase()
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
